6c426f4ab31a19940053360aafb1132d65c7f119
mskrzypk <mskrzypk@mion.elka.pw.edu.pl>
2017-10-29 17:45:10 +0100

    adding CSS file- no code yet- .java change

--- src/login/Login.java
+++ src/login/Login.java
@@ -78,6 +78,7 @@ public class Login extends Application {
         
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
+        scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
         primaryStage.show();
     }
 
